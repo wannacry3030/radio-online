325b3d592d7ac7749c12ec264c6acdbf9b60ff44
wannacry3030 <wannacry.bat@proton.me>
2022-12-23 23:04:08 -0300

    new file:   player 2/cover.png

--- player 2/main.js
+++ player 2/main.js
@@ -1,6 +1,14 @@
 const tracks = ["song1.mp3", "song2.mp3", "song3.mp3"];
 
 let currentTrack = 0;
+const player = document.querySelector("#player");
+const prevButton = document.querySelector("#prev-button");
+const nextButton = document.querySelector("#next-button");
+
+player.src = tracks[currentTrack];
+
+prevButton.addEventListener("click", playPrevious);
+nextButton.addEventListener("click", playNext);
 
 function playNext() {
   currentTrack = (currentTrack + 1) % tracks.length;
